7755da0aca59cf1bea3d0e1048d0e7a5c136d942
Mario Simão <mariosimao@poli.ufrj.br>
2022-07-19 12:51:19 -0300

    Adicionando questão 6

---
+++ questao6p2/questao6p2.m
@@ -0,0 +1,113 @@
+% frequencia de amostragem
+FS = 10000;
+
+% frequencia do fim da primeira faixa de rejeição dividida por pi
+Wr1 = 2*1000/FS;                          
+
+% frequencia do inicio da faixa de passagem dividida por pi
+Wp1 = 2*1500/FS;                           
+
+% frequencia do fim da faixa de passagem dividida por pi
+Wp2 = 2*2500/FS;
+
+% frequencia do inicio da segunda faixa de rejeição dividida por pi
+Wr2 = 2*3000/FS;
+
+rs1_db = -43; % para dar um margem
+rs2_db = -53; % para dar um margem
+rp_db = 1;
+
+rs1 = 10^(rs1_db/20);
+rs2 = 10^(rs2_db/20);
+rp = 10^(rp_db/20) - 1;
+
+dev = [10^(rs1_db/20) (10^(rp_db/20)-1)/(10^(rp_db/20)+1) 10^(rs2_db/20)];
+[n,fo,ao,w] = firpmord([Wr1 Wp1 Wp2 Wr2], [0 1 0], dev);
+b = firpm(n, fo, ao, w);               % b: coef. do numerador de H(z)
+a = 1;
+
+% carregando o sinais de audio
+[~,Fs] = audioread('..\musica.wav');
+tempo_inicial_segundos = 85;
+tempo_inicial_amostras = tempo_inicial_segundos*Fs;
+start = tempo_inicial_amostras;
+samples = [start,start+5*Fs];
+clear Fs
+[x1,sr1] = audioread('..\musica.wav',samples);
+
+[~,Fs] = audioread('..\voz.wav');
+samples = [1,5*Fs];
+clear Fs
+[x2,sr2] = audioread('..\voz.wav',samples);
+
+x1 = x1(:,1);
+x2 = x2(:,1);
+
+% --- Alterando a freq de amostragem para FS:
+if sr1 ~= FS
+   x1 = resample(x1, FS, sr1);
+end
+if sr2 ~= FS
+   x2 = resample(x2, FS, sr2);
+end
+
+% Usando o filtro da questao 1 nos sinais:
+y1 = filter(b,a,x1);
+y2 = filter(b,a,x2);
+
+% Descomente para ouvir os sinais:
+%soundsc(y1,FS);
+%soundsc(y2,FS);
+
+% salva os resultados:
+audiowrite('musica_filtrada.wav',y1,FS);
+audiowrite('voz_filtrada.wav',y2,FS);
+
+% espectrogramas
+N = 256;
+window = hamming(N);
+Noverlap = N/2;
+
+[sx1,fx1,tx1] = spectrogram(x1,window,Noverlap,N);
+[sx2,fx2,tx2] = spectrogram(x2,window,Noverlap,N);
+[sy1,fy1,ty1] = spectrogram(y1,window,Noverlap,N);
+[sy2,fy2,ty2] = spectrogram(y2,window,Noverlap,N);
+
+% Exibe os espectrogramas originais e filtrados:
+f = figure('units', 'centimeters','position',[3,2,30,15]);
+figure(f);
+h = tiledlayout(2,2, 'Padding', 'compact');
+nexttile
+surf(tx1,fx1,20*log10(abs(sx1)),'LineStyle','none')
+view(0,90)
+title('Espectrograma de Potência do sinal musical original')
+xlim([-inf inf]);
+ylim([-inf inf]);
+ylabel('frequência [Hz]')
+colormap bone
+colorbar
+nexttile
+surf(tx2,fx2,20*log10(abs(sx2)),'LineStyle','none')
+view(0,90)
+title('Espectrograma de Potência do sinal de voz original')
+xlim([-inf inf])
+ylim([-inf inf])
+colormap bone
+colorbar
+nexttile
+surf(ty1,fy1,20*log10(abs(sy1)),'LineStyle','none')
+title('Espectrograma de Potência do sinal musical filtrado')
+view(0,90)
+xlim([-inf inf])
+ylim([-inf inf])
+ylabel('frequência [Hz]')
+colormap bone
+colorbar
+nexttile
+surf(ty2,fy2,20*log10(abs(sy2)),'LineStyle','none')
+title('Espectrograma de Potência do sinal de voz filtrado')
+view(0,90)
+xlim([-inf inf])
+ylim([-inf inf])
+colormap bone
+colorbar
---
+++ questao6p3/questao6p3.m
@@ -0,0 +1,96 @@
+% frequencia de amostragem
+Fs = 10000;
+
+% frequencia do fim da primeira faixa de rejeição dividida por pi
+Wr1 = 2*1000/Fs;                          
+
+% frequencia do inicio da faixa de passagem dividida por pi
+Wp1 = 2*1500/Fs;                           
+
+% frequencia do fim da faixa de passagem dividida por pi
+Wp2 = 2*2500/Fs;
+
+% frequencia do inicio da segunda faixa de rejeição dividida por pi
+Wr2 = 2*3000/Fs;
+
+rs1_db = -43; % para dar um margem
+rs2_db = -53; % para dar um margem
+rp_db = 1;
+
+rs1 = 10^(rs1_db/20);
+rs2 = 10^(rs2_db/20);
+rp = 10^(rp_db/20) - 1;
+
+dev = [10^(rs1_db/20) (10^(rp_db/20)-1)/(10^(rp_db/20)+1) 10^(rs2_db/20)];
+[n,fo,ao,w] = firpmord([Wr1 Wp1 Wp2 Wr2], [0 1 0], dev);
+b = firpm(n, fo, ao, w);               % b: coef. do numerador de H(z)
+a = 1;
+
+% carregando o sinais de audio
+[~,Fs] = audioread('../musica.wav');
+tempo_inicial_segundos = 85;
+tempo_inicial_amostras = tempo_inicial_segundos*Fs;
+start = tempo_inicial_amostras;
+samples = [start,start+5*Fs];
+clear Fs
+[x1,sr1] = audioread('../musica.wav',samples);
+
+[~,Fs] = audioread('../voz.wav');
+samples = [1,5*Fs];
+clear Fs
+[x2,sr2] = audioread('../voz.wav',samples);
+
+x1 = x1(:,1);
+x2 = x2(:,1);
+
+% --- Alterando a freq de amostragem para FS:
+if sr1 ~= FS
+   x1 = resample(x1, FS, sr1);
+end
+if sr2 ~= FS
+   x2 = resample(x2, FS, sr2);
+end
+
+% Potencia dos sinais
+pot_x2 = sum(x2.^2)/length(x2);
+pot_x1 = sum(x1.^2)/length(x1);
+
+% SNR = 10*log(pot_sinal/pot_ruido):
+SNR = 10; 
+pot_ruido_x2 = pot_x2/10^(SNR/10);
+pot_ruido_x1 = pot_x1/10^(SNR/10);
+
+% pot_ruido = desvio_padrao^(1/2):
+desvio_x2 = pot_ruido_x2^(1/2);
+desvio_x1 = pot_ruido_x1^(1/2);
+
+% Colocando ruido branco nos sinais
+contaminado_x2 = x2 + desvio_x2.*randn(length(x2),1);
+contaminado_x1 = x1 + desvio_x1.*randn(length(x1),1);
+
+t_contaminado_x2 = (1:length(contaminado_x2))/FS;
+t_contaminado_x1 = (1:length(contaminado_x1))/FS;
+
+% Usando o filtro da questao 1 nos sinais:
+y1 = filter(b,a,contaminado_x1);
+y2 = filter(b,a,contaminado_x2);
+
+% salva os resultados:
+audiowrite('musica_contaminada_filtrada.wav',y1,FS);
+audiowrite('voz_contaminada_filtrada.wav',y2,FS);
+
+% Espectogramas
+N = 256;
+window = hamming(N);
+Noverlap = N/2;
+figure('units', 'centimeters', 'position', [2, 2, 23, 10])
+spectrogram(y1, window, Noverlap, N, 'yaxis')
+title('musica.wav contaminada e filtrada')
+colormap bone
+figure('units', 'centimeters', 'position', [2, 2, 23, 10])
+spectrogram(y2, window, Noverlap, N, 'yaxis')
+title('voz.wav contaminada e filtrada')
+colormap bone
+
+%soundsc(y1, FS)
+%soundsc(y2, FS)
